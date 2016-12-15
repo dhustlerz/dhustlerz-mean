16b9390e349906940b89ca83836b31ac0e3f143f
Jashanpreet Singh <jsingh@corp.ctv.ca>
2016-12-15 16:14:17 -0500

    backend work

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -6,34 +6,39 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+  function MainController($timeout, webDevTec, toastr, $scope) {
+    // var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1481817960718;
-    vm.showToastr = showToastr;
+    // vm.awesomeThings = [];
+    // vm.classAnimation = '';
+    // vm.creationDate = 1481817960718;
+    // vm.showToastr = showToastr;
 
-    activate();
+    // activate();
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    // function activate() {
+    //   getWebDevTec();
+    //   $timeout(function() {
+    //     vm.classAnimation = 'rubberBand';
+    //   }, 4000);
+    // }
+
+    // function showToastr() {
+    //   toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    //   vm.classAnimation = '';
+    // }
+
+    // function getWebDevTec() {
+    //   vm.awesomeThings = webDevTec.getTec();
+
+    //   angular.forEach(vm.awesomeThings, function(awesomeThing) {
+    //     awesomeThing.rank = Math.random();
+    //   });
+    // }
+    $scope.postMessage = function () {
+      alert('myMessageFront Posted');
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
+
